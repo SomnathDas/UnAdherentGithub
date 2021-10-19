ab20386bead65d95b5080da9be75b38f100263b1
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2021-10-19 09:03:24 +0530

    Update List.js

--- src/List.js
+++ src/List.js
@@ -4,9 +4,11 @@ import Card from "./Card";
 export default function List({ list }) {
   return (
     <section className="card-list">
-      {list.map((x) => (
-        <Card githubUserId={x.id} username={x.login} />
-      ))}
+      {list.length === 0 ? (
+        <h2>No Non-Followers Found</h2>
+      ) : (
+        list.map((x) => <Card githubUserId={x.id} username={x.login} />)
+      )}
     </section>
   );
 }
