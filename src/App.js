f6054d7ad8b629d875b935ea907012b2e0aa7b23
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2021-10-19 09:00:44 +0530

    Major Visual Fixes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 import Loading from "./Loading";
+import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
   const [isFetched, setIsFetched] = useState(false);
   const [nonFollowersList, setNonFollowersList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const changeHandler = (event) => {
     setUsername(event.target.value);
   };
@@ -21,24 +23,36 @@ function App() {
         )
       ).json();
       setNonFollowersList(data);
-      setIsFetched(true);
       setIsLoading(false);
+      setIsFetched(true);
     } catch (error) {
       console.log(error);
+      setIsError(true);
     }
   };
 
   return (
     <>
-      {isLoading === true ? <Loading /> : ""}
-      {isFetched === false && isLoading === false ? (
+      {isLoading === true && isFetched === false && isError === false ? (
+        <Loading />
+      ) : isError === true ? (
+        <div className="error-moment">
+          <h2>Error! Please Try Again.</h2>
+          <ul className="to-dolist">
+            <li>Make Sure You have entered correct username.</li>
+            <li>Also Take care of case-sensistivity.</li>
+            <li>Please do not try to spam :)</li>
+            <li>Eating lava is good for apple.</li>
+          </ul>
+        </div>
+      ) : isLoading === false && isFetched === true && isError === false ? (
+        <List list={nonFollowersList} />
+      ) : (
         <Form
           username={username}
           changeHandler={changeHandler}
           onClickHandler={fetchNonFollowersList}
         />
-      ) : (
-        <List list={nonFollowersList} />
       )}
     </>
   );
